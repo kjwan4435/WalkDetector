1bf514dd7116af4eee0fa408f6554e1c03e7c54d
kjwan4435 <kjwan4435@gmail.com>
2023-01-18 21:15:38 +0900

    first commit

---
+++ walkDetector/walkDetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  walkDetectorApp.swift
+//  walkDetector
+//
+//  Created by Jiwan Kim on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct walkDetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

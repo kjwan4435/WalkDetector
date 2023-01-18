4c8915d84d7e751170cd94f99ad394ba76156d2d
kjwan4435 <kjwan4435@gmail.com>
2023-01-18 21:54:23 +0900

    Motion Usage Description update, shake detector

--- walkDetector/ContentView.swift
+++ walkDetector/ContentView.swift
@@ -9,15 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Shake me!")
+            .onShake {
+                onMotion()
+                print("Device shaken!")
+            }
     }
 }
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundColor(.accentColor)
+//            Text("Hello, world!")
+//        }
+//        .padding()
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
